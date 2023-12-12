dc0260797ccea9c32729da93baf064fa6cda79f2
Jaysa Lendio <22104713@usc.edu.ph>
2023-12-12 16:34:08 +0800

    First Commmit

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Showusers()
+        {
+            friendsEntities fe = new friendsEntities();
+            var userList = (from a in fe.users
+                            select a).ToList();
+
+            ViewData["UserList"] = userList;
+            return View();
+            
+        }
+        public ActionResult AddUserToDatabase(FormCollection fc)
+        {
+            String firstName = fc["firstname"];
+            String lastName = fc["lastname"];
+            String email = fc["email"];
+            int age = Convert.ToInt16(fc["age"]);
+            String address = fc["address"];
+            String gender = fc["gender"];
+            String password = fc["password"];
+
+            user use = new user();
+            use.first_name = firstName;
+            use.last_name = lastName;
+            use.email = email;
+            use.password = password;
+            use.role_id = 1;
+
+
+
+            friendsEntities fe = new friendsEntities();
+            fe.users.Add(use);
+            fe.SaveChanges();
+
+            //insert the code that will save these information to the DB
+
+            return RedirectToAction("ShowUsers");
+        }
+
+    }
+}
